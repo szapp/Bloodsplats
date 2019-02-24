940d5fd6e7fda5638300d40fa03c5a5ad540bf38
szapp <dev.szapp@gmail.com>
2019-02-24 13:59:11 +0100

    Simplify initialization

--- Ninja/Bloodsplats/Content/NinjaInit.d
+++ Ninja/Bloodsplats/Content/NinjaInit.d
@@ -5,9 +5,7 @@ func void Ninja_Bloodsplats_Init() {
     // Wrapper for "LeGo_Init" to ensure correct LeGo initialization without breaking the mod
     LeGo_MergeFlags(/* LeGo_Bloodsplats = */ LeGo_FrameFunctions | LeGo_Random | LeGo_Anim8);
 
-    // Replace LeGo_Bloodsplats initialization with a volatile hook to prevent writing the FF permanently to game saves
-    if (!(_LeGo_Flags & LeGo_Bloodsplats)) {
-        HookEngineF(oCGame__Render, 7, _Bloodsplats_Loop);
-        _LeGo_Flags = _LeGo_Flags | LeGo_Bloodsplats;
-    };
+    // Initialize LeGo_Bloodsplats explicitly
+    FF_ApplyOnce(_Bloodsplats_Loop);
+    _LeGo_Flags = _LeGo_Flags | LeGo_Bloodsplats;
 };
